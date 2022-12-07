5e7589610b36c449b9824c5df7d4042ccd6ca750
İrem Aksoy <60525860+iremaksoy14@users.noreply.github.com>
2022-12-07 14:08:00 +0300

    HEADER BUG FİX

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { AppRoute } from '../../constants/constants';
 
 function Logo(): JSX.Element {
   return (
-    <div className="logo">
-      <Link className="logo__link" to={AppRoute.Root}>
-        <span className="logo__letter logo__letter--1">W</span>
-        <span className="logo__letter logo__letter--2">T</span>
-        <span className="logo__letter logo__letter--3">W</span>
-      </Link>
-    </div>
+    <div className="logo"></div>
   );
 }
 
